5781ac5b23b5f37990db7b87efe310f701feb7f7
Color-Kat <katik7979@gmail.com>
2021-12-11 16:59:42 +0300

    litium gidrig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Renderer from "./classes/Renderer";
 
-type coords = {x: number, y: number};
+type coords = { x: number, y: number };
 
 let canvas = document.getElementById('space') as HTMLCanvasElement;
 let w = canvas.width = window.innerWidth;
@@ -11,30 +11,60 @@ let ctx = canvas.getContext('2d');
 let renderer = new Renderer(ctx, w, h, '#0f1c3d');
 
 let atoms = [
+    {
+        name: 'H',
+        color: 'white',
+        mass: 1,
+        charge: 0,
+        Z: 1,
+        coords: {x: 400, y: 300},
+        taken: false
+    },
     // {
+    //     name: 'H',
     //     color: 'white',
-    //     mass : 1,
+    //     mass: 1,
+    //     charge: 0,
     //     Z: 1,
-    //     coords: {x: 200, y: 300}
+    //     coords: {x: 300, y: 400},
+    //     taken: false
     // },
-    {
-        color: 'gray',
-        mass : 7,
-        Z: 3,
-        coords: {x: 400, y: 400}
-    },
-    {
-        color: 'red',
-        mass : 16,
-        Z: 8,
-        coords: {x: 400, y: 700}
-    },
     // {
-    //     color: 'black',
-    //     mass : 12,
-    //     Z: 6,
-    //     coords: {x: 100, y: 700}
+    //     name: 'H',
+    //     color: 'white',
+    //     mass: 1,
+    //     charge: 0,
+    //     Z: 1,
+    //     coords: {x: 400, y: 400},
+    //     taken: false
     // },
+    {
+        name: 'Li',
+        color: 'gray',
+        mass: 7,
+        charge: 0,
+        Z: 3,
+        coords: {x: 500, y: 400},
+        taken: false
+    },
+    {
+        name: 'O',
+        color: 'red',
+        mass: 16,
+        charge: 0,
+        Z: 8,
+        coords: {x: 200, y: 400},
+        taken: false
+    },
+    {
+        name: 'C',
+        color: 'black',
+        mass: 12,
+        charge: 0,
+        Z: 6,
+        coords: {x: 200, y: 700},
+        taken: false
+    },
 ];
 
 
@@ -42,42 +72,104 @@ function offset(min: number, max: number) {
     return Math.random() * (max - min) + min;
 }
 
-function getDistance(coordsA: coords, coordsB: coords){
-    return Math.sqrt((coordsA.x-coordsB.x)**2 + (coordsA.y-coordsB.y)**2);
+function getDistance(coordsA: coords, coordsB: coords) {
+    return Math.sqrt((coordsA.x - coordsB.x) ** 2 + (coordsA.y - coordsB.y) ** 2);
 }
 
-renderer.render((ctx)=>{
+function getCountOfMissElectrons(atom: {mass: number, Z: number}): number{
+    let e = atom.Z;
 
-    for (let i = 0; i<atoms.length; i++){
-    // for (let i in atoms){
-    //     console.log(i)
+    if (e <= 2) return 2-e;
+    if (e <= 10) return 10-e;
+    return 0;
+}
+
+function getCountOfLastElectrons(atom: {mass: number, Z: number}): number{
+    let e = atom.Z;
+
+    if (e <= 2) return e;
+    if (e <= 10) return e-2;
+    return 0;
+}
+
+
+renderer.render((ctx) => {
+    for (let i = 0; i < atoms.length; i++) {
+        // for (let i in atoms){
+        //     console.log(i)
         ctx.beginPath();
 
         let atomA = atoms[i];
-        let atomB = atoms[i+1] ?? null;
 
-        if(atomB) {
-            console.log(atomB.coords)
+        for (let j = 0; j < atoms.length; j++) {
+            if (i == j) continue; // this is current atom
+
+            let atomB = atoms[j];
+
             let distance = getDistance(atomA.coords, atomB.coords);
-            let distX = atomA.coords.x-atomB.coords.x;
-            let distY = atomA.coords.y-atomB.coords.y;
+            let distX = atomA.coords.x - atomB.coords.x;
+            let distY = atomA.coords.y - atomB.coords.y;
 
-            atomA.coords.x += (distX > 0 ? distX : -distX)/distance *2;
-            atomA.coords.y += (distY > 0 ? distY : -distX)/distance *2;
-            atomB.coords.x += (distY > 0 ? distY : distX)/distance *2;
-            atomB.coords.y += (distY > 0 ? distY : distX)/distance *2;
+            // by electrons
+            if (
+                getCountOfMissElectrons(atomA) == getCountOfLastElectrons(atomB)||
+                getCountOfMissElectrons(atomB) == getCountOfLastElectrons(atomA)
+            ) {
+                let moveX = distX / distance;
+                let moveY = distY / distance;
 
-            atoms[i+1] = atomB;
+                if(distance < 30) {
+                    atomA.taken = true;
+                    atomB.taken = true;
+                }
+
+                atomA.coords.x += -moveX;
+                atomA.coords.y += -moveY;
+                atomB.coords.x += moveX ;
+                atomB.coords.y += moveY ;
+            }
+
+            // ions
+            if (distance > 30) {
+                let moveX = distX / distance;
+                let moveY = distY / distance;
+
+                // attracted
+                if(atomA.charge + atomB.charge === 0 && atomA.charge !== 0 && atomB.charge !== 0){
+                    atomA.coords.x += -moveX;
+                    atomA.coords.y += -moveY;
+                    atomB.coords.x += moveX ;
+                    atomB.coords.y += moveY ;
+                }
+
+                // repel
+                if (atomA.charge === atomB.charge && atomA.charge !== 0) {
+                    atomA.coords.x += moveX;
+                    atomA.coords.y += moveY;
+                    atomB.coords.x += -moveX ;
+                    atomB.coords.y += -moveY ;
+                }
+            } else {
+                // atomA.charge = 0;
+                // atomB.charge = 0;
+            }
+
+            atoms[j] = atomB;
         }
-        else {
-            // atomA.coords.x +=offset (-5,5);
-            // atomA.coords.y +=offset (-5,5);
-        }
+        atomA.coords.x += offset(-5,5);
+        atomA.coords.y += offset(-5,5);
 
         atoms[i] = atomA;
 
         ctx.fillStyle = atomA.color;
-        ctx.arc(atomA.coords.x, atomA.coords.y,50, 0, 360);
+        ctx.arc(atomA.coords.x, atomA.coords.y, 50, 0, 360);
         ctx.fill();
+
+        ctx.font = '50px Arial';
+        ctx.fillStyle = 'blue';
+        ctx.textAlign = "center";
+        ctx.fillText(atomA.name,atomA.coords.x, atomA.coords.y);
+        ctx.font = '30px Arial';
+        ctx.fillText(atomA.charge.toString(),atomA.coords.x, atomA.coords.y+30);
     }
 });
